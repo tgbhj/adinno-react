3e85b98a7cb9a81b48ec708675cb61a5e1c3897d
tgbhj <j1060381781@qq.com>
2020-03-17 15:26:32 +0800

    commit

--- src/components/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Result, Button } from 'antd'
-
-function NotFound() {
-    return <Result status="404" title="404" subTitle="页面不存在" extra={<Button type="primary" href='/'>返回首页</Button>} />
-}
-
-export default NotFound
--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import index from '../views/index'
 import zhCN from 'antd/es/locale/zh_CN'
 import { ConfigProvider } from 'antd'
-import NotFound from '../components/NotFound'
 
 const Routers = () => (
     <ConfigProvider locale={zhCN}>
         <Router>
             <Switch>
                 <Route exact path="/" component={index} />
-                <Route component={NotFound} />
             </Switch>
         </Router>
     </ConfigProvider>
